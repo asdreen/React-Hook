1c8e0e3dd4a0597a6bc26ea55d10998f4da988a1
Asdren JERLIU <114486010+asdreen@users.noreply.github.com>
2022-11-29 17:25:24 +0100

    update

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import BookList from "./components/BookList";
+import fantasyBooks from "./fantasyBooks.json";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BookList books={fantasyBooks} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
